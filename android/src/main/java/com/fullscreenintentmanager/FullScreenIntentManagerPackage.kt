5ad322e3f07a5266691e95b107a7984e0c16c8d3
Ritesh Shukla <Ritesh.shukla2@bfhl.in>
2024-08-05 22:38:09 +0530

    chore: initial commit

---
+++ android/src/main/java/com/fullscreenintentmanager/FullScreenIntentManagerPackage.kt
@@ -0,0 +1,17 @@
+package com.fullscreenintentmanager
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class FullScreenIntentManagerPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(FullScreenIntentManagerModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  FullScreenIntentManagerExample
+//
+
+import Foundation
